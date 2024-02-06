e8c824227e4c896517ae661ea59ef31e96f3c672
murphyjl <murphyjl@rose-hulman.edu>
2024-02-05 20:05:25 -0500

    creating objects

--- src/mainApp/Barriers.java
+++ src/mainApp/Barriers.java
@@ -7,18 +7,20 @@ public class Barriers {
 	
 	private static final int STANDARD_BAR_SIZE = 40;
 	private int barrierX, barrierY;
+	private boolean isElectric;
 	
-	public Barriers(int barrierX, int barrierY) {
+	public Barriers(int barrierX, int barrierY, boolean isElectric) {
 		this.barrierX=barrierX;
 		this.barrierY=barrierY;
+		this.isElectric=isElectric;
 	}
 	
 	public void paintBarrier(Graphics g2) {
-		g2.setColor(Color.GREEN);
-		g2.fillRect(this.barrierX, this.barrierY, STANDARD_BAR_SIZE, STANDARD_BAR_SIZE);
-	}
-	public void paintELectricBarrier(Graphics g2) {
-		g2.setColor(Color.RED);
+		if (this.isElectric==true) {
+			g2.setColor(Color.RED);
+		} else {
+			g2.setColor(Color.GREEN);
+		}
 		g2.fillRect(this.barrierX, this.barrierY, STANDARD_BAR_SIZE, STANDARD_BAR_SIZE);
 	}
 }
--- src/mainApp/FileReader.java
+++ src/mainApp/FileReader.java
@@ -2,9 +2,13 @@ package mainApp;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class FileReader {
+	private static ArrayList<Barriers> barriers=new ArrayList<>();
+	private static ArrayList<Coin> coins=new ArrayList<>();
 
 	static void readFile(String filename) throws InvalidLevelFormatException {
 		String elementsConfiguration = "";
@@ -19,15 +23,17 @@ public class FileReader {
 				for (int i = 0; i < line.length(); i++) {
 					if (line.charAt(i) == 'C') {
 						System.out.println("Create coin! at " + i * 50);
-
+						coins.add(new Coin(i, i));
 					}
 					if (line.charAt(i) == 'B') {
 
 						System.out.println("Create Barrier at " + i * 50);
+						barriers.add(new Barriers(i, i, false));
 
 					}
 					if (line.charAt(i) == 'E') {
 						System.out.println("Create Electric Barrier");
+						barriers.add(new Barriers(i, i, true));
 					}
 				}
 				if (line.length() > 31) {

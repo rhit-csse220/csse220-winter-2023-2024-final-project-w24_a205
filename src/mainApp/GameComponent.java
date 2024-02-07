c8e335a305f4412b4cfb93ffaf28e6fae8732656
murphyjl <murphyjl@rose-hulman.edu>
2024-02-06 19:59:58 -0500

    missile stuff

--- src/mainApp/GameComponent.java
+++ src/mainApp/GameComponent.java
@@ -13,11 +13,24 @@ public class GameComponent extends JComponent {
 	Boolean isElectric;
 	private ArrayList<Barriers> barriers=new ArrayList<>();
 	private ArrayList<Coin> coins=new ArrayList<>();
+	private ArrayList<Missiles> missiles=new ArrayList<>();
 	
 	public void listsIn(ArrayList bars, ArrayList coins) {
 		this.barriers = bars;
 		this.coins = coins; 
 	}
+	
+	public void updateMissiles() {
+		for(Missiles missile: this.missiles) {
+			if (missile.move()==true) {
+				int missileX=this.getX()+missile.getX();
+				int missileY=this.getY();
+				this.missiles.remove(missile);
+				this.missiles.add(new Missiles(missileX, missileY));
+			}
+		}
+	}
+
 	
 	
 	@Override
--- src/mainApp/Missiles.java
+++ src/mainApp/Missiles.java
@@ -5,12 +5,22 @@ import java.awt.Graphics;
 
 public class Missiles {
 	
-	private static final int MISSILE_SIZE = 50;
+	private static final int MISSILE_SIZE = 20;
 	private int missileX, missileY;
+	private double xVelocity = 5;
 	
-	public Missiles(int missleX, int missleY) {
+	public Missiles(int missileX, int missileY) {
 		this.missileX=missileX;
 		this.missileY=missileY;
+	}
+	
+	public boolean move() {
+		this.missileX -= this.xVelocity;
+		return this.missileX <= 0;
+	}
+	
+	public int getX() {
+		return this.missileX;
 	}
 	
 	public void paint(Graphics g2) {
